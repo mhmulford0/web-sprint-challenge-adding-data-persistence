332ce6778ff313428d245209eaadfe7b23ba715c
Matthew Mulford <mhmulford0@gmail.com>
2020-10-09 11:55:34 -0500

    project router complete

--- routes/projectModel.js
+++ routes/projectModel.js
@@ -15,8 +15,32 @@ const getProjectTasks = (id) => {
     .where("project.id", id);
 };
 
+const getProjectResources = (id) => {
+  return db("project")
+    .join("resources", "resources.project_id", "=", "project.id")
+    .select("resources.name", "resources.desc");
+};
+
+const addProject = (data) => {
+  const { name, desc, complete } = data;
+  return db("project").insert({
+    name,
+    desc,
+    complete,
+  });
+};
+
+const updateProject = (id, data) => {
+  return db("project").update({
+    complete: data,
+  });
+};
+
 module.exports = {
   getProjects,
   getProject,
   getProjectTasks,
+  getProjectResources,
+  addProject,
+  updateProject,
 };
--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -2,7 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getProjects, getProject, getProjectTasks } = require("./projectModel");
+const {
+  getProjects,
+  getProject,
+  getProjectTasks,
+  getProjectResources,
+  addProject,
+  updateProject,
+} = require("./projectModel");
 
 router.get("/", async (req, res) => {
   try {
@@ -33,4 +40,39 @@ router.get("/:id/tasks", async (req, res) => {
   }
 });
 
+router.get("/:id/resources", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const projResources = await getProjectResources();
+    res.status(200).json({ data: projResources });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const data = req.body;
+
+  try {
+    const result = await addProject(data);
+    res.status(201).json({ message: "project added" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error adding project" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { complete } = req.body;
+
+  try {
+    const updates = await updateProject(id, complete);
+    res.status(201).json({ message: "project status updated" });
+  } catch (error) {
+    res.status(500).json({ message: "error updating project" });
+  }
+});
 module.exports = router;
