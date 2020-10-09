0fba39da9a44b9e9bab338320dce183a677695af
Matthew Mulford <mhmulford0@gmail.com>
2020-10-09 11:27:33 -0500

    getting projects by id

--- routes/projectModel.js
+++ routes/projectModel.js
@@ -4,6 +4,19 @@ const getProjects = () => {
   return db("project");
 };
 
+const getProject = (id) => {
+  return db("project").where({ id });
+};
+
+const getProjectTasks = (id) => {
+  return db("project")
+    .join("tasks", "tasks.project_id", "=", "project.id")
+    .select("tasks.desc", "tasks.notes", "tasks.complete")
+    .where("project.id", id);
+};
+
 module.exports = {
   getProjects,
+  getProject,
+  getProjectTasks,
 };
--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -2,12 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getProjects } = require("./projectModel");
+const { getProjects, getProject, getProjectTasks } = require("./projectModel");
 
 router.get("/", async (req, res) => {
   try {
     const projects = await getProjects();
     res.status(200).json({ data: projects });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const project = await getProject(id);
+    res.status(200).json({ data: project });
+  } catch (error) {}
+});
+
+router.get("/:id/tasks", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const projTasks = await getProjectTasks(id);
+    res.status(200).json({ data: projTasks });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "error" });
